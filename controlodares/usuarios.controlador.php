4b12a190367da43042752478cbbd388d3be96691
RICHARD SASVIN <richard.ortiz@digicom.com.gt>
2023-10-22 15:04:22 -0600

    controlador usuario terminado

--- controlodares/usuarios.controlador.php
+++ controlodares/usuarios.controlador.php
@@ -134,4 +134,120 @@ class ControladorUsuarios{
                 <script>';
         }
     }
+
+    static public function ctrEditarUsuario(){
+        if (isset($_POST["editarUsuario"])) {
+            if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["editarNombre"])) {
+
+                $tabla = "usuarios";
+
+                //valido
+                if ($_POST["editarPassword"] != "") {
+
+					if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["editarPassword"])) {
+
+						$encriptar = crypt($_POST["editarPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+
+					} else {
+
+						echo '<script>
+
+								swal({
+									  type: "error",
+									  title: "¡La contraseña no puede ir vacía o llevar caracteres especiales!",
+									  showConfirmButton: true,
+									  confirmButtonText: "Cerrar"
+									  }).then(function(result) {
+										if (result.value) {
+
+										window.location = "usuarios";
+
+										}
+									})
+
+						  	</script>';
+
+						return;
+
+					}
+
+				}else{
+                    $encriptar = $_POST["passwordActual"];
+                }
+
+                $datos = array("nombre" => $_POST["editarNombre"],
+                                "usuario" => $_POST["editarUsuario"],
+                                "password" => $encriptar,
+                                "perfil" => $_POST["editarPerfil"]);
+                
+                $respuesta = ModeloUsuarios::mdlEditarUsuario($tabla, $datos);
+
+                if($respuesta == "ok"){
+                    echo '<script>
+
+					swal({
+						  type: "success",
+						  title: "El usuario ha sido editado correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result) {
+									if (result.value) {
+
+									window.location = "usuarios";
+
+									}
+								})
+
+					</script>';
+                }
+            }else{
+                echo '<script>
+
+					swal({
+						  type: "error",
+						  title: "¡El nombre no puede ir vacío o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result) {
+							if (result.value) {
+
+							window.location = "usuarios";
+
+							}
+						})
+
+			  	</script>';
+            }
+        }
+    }
+
+    static public function ctrBorrarUsuario(){
+        if(isset($_GET["idUsuario"])){
+
+            $tabla = "usuarios";
+            $datos = $_GET["idUsuario"];
+
+            $respuesta = ModeloUsuarios::mdlEliminarUsuario($tabla, $datos);
+
+            if($respuesta == "ok"){
+                echo '<script>
+
+				swal({
+					  type: "success",
+					  title: "El usuario ha sido borrado correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar",
+					  closeOnConfirm: false
+					  }).then(function(result) {
+								if (result.value) {
+
+								window.location = "usuarios";
+
+								}
+							})
+
+				</script>';
+            }
+        }
+    }
 }
